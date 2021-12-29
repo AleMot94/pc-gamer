9dffb994d81aaa51d183ce14f5ed82bed085cbaf
AleMot94 <Alector666@gmai.com>
2021-12-29 19:15:53 -0300

    se agrego archivo .js

---
+++ main.js
@@ -0,0 +1,13 @@
+const productos = [];
+
+class Producto {
+    constructor(id, nombre, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+productos.push(new Producto(1, intel core i3, $150));
+productos.push(new Producto(2, intel core i5, $190));
+productos.push(new Producto(3, intel core i7, $230));
